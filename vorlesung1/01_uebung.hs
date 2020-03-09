169f7676293baf9ce36a6fe7ec720acdc56790de
maSchoeller <git@marvinschoeller.de>
2020-03-09 11:54:58 +0100

    update

--- vorlesung1/01_uebung.hs
+++ vorlesung1/01_uebung.hs
@@ -8,13 +8,18 @@ mylist = [1,2,3,4,4,5,67,8]
 --  Übung 1b)
 myStr =  "Hallo"
 
-stripList ( _ : str) = reverse $ tail $ reverse str
+stripList ls = stripLastElement $ tail ls
+stripLastElement ( x : str : []) = x : []
+stripLastElement (x : ls) = x : (stripLastElement ls)
 
+-- stripList (_ : list) = reverse $ tail $ reverse list
+ 
 
 -- Übung 1c)
 isPalindrom []  = True
-isPalindrom (_ : []) = False
+isPalindrom (_ : []) = True
 isPalindrom l = if (head l) == (lastElement l) then isPalindrom $ stripList l else False
 
 mySecondStr = "alla"
 mySecondStr2 = "alla2"
+mySecondStr3 = "alala"
